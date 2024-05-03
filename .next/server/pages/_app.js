b8f432e7c88ef2677402589b54b030f115b5fba2
changhak0310 <dlckdgkr0320@naver.com>
2024-05-03 22:08:53 +0900

    test

---
+++ .next/server/pages/_app.js
@@ -0,0 +1,1 @@
+(()=>{var r={};r.id=888,r.ids=[888],r.modules={3893:(r,e,s)=>{"use strict";s.r(e),s.d(e,{default:()=>u});var t=s(997);function u({Component:r,pageProps:e}){return t.jsx(r,{...e})}s(108)},108:()=>{},997:r=>{"use strict";r.exports=require("react/jsx-runtime")}};var e=require("../webpack-runtime.js");e.C(r);var s=e(e.s=3893);module.exports=s})();
