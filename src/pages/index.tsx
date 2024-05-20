d27ca6b29190a991d33a9d9b7a47bfd0e97c491e
changhak0310 <dlckdgkr0320@naver.com>
2024-05-20 17:04:49 +0900

    refactory: 불필요한 코드 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -214,7 +214,6 @@ export default function Home() {
   React.useEffect(() => {
     const CT = new ChannelTalk();
     CT.boot({ pluginKey: "a91cb56e-c0c2-458f-a6c1-4a8ba3a34c93" });
-
     return () => {
       CT.shutdown();
     };
